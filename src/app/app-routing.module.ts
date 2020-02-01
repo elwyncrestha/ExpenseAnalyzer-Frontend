f1715c6509dd781baaa917419ecc4ea1ee04441c
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-01 20:11:44 +0545

    Refactor: Added authentication related components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthBaseComponent} from './component/auth-base/auth-base.component';
+import {LoginComponent} from './component/login/login.component';
+import {ForgotPasswordComponent} from './component/forgot-password/forgot-password.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {
+    path: '', component: AuthBaseComponent, children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+        // TODO Add `LoginGuard`
+      },
+      {
+        path: 'forgotPassword',
+        component: ForgotPasswordComponent,
+        // TODO Add `LoginGuard`
+      },
+    ],
+  },
+  {path: '**', redirectTo: 'login'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthBaseComponent } from './component/auth-base/auth-base.component';
+import { LoginComponent } from './component/login/login.component';
+import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthBaseComponent,
+    LoginComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
