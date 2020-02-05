e234b0f0789d7df1a193124cc885c1cb6ba6e94a
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-05 13:17:36 +0545

    Feature: Expense Type Visualization

--- src/app/module/main/module/data-visualization/data-visualization.module.ts
+++ src/app/module/main/module/data-visualization/data-visualization.module.ts
@@ -2,15 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CategoryChartComponent} from './component/category-chart/category-chart.component';
 import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {TransactionTypeChartComponent} from './component/transaction-type-chart/transaction-type-chart.component';
 
 
 @NgModule({
-  declarations: [CategoryChartComponent],
+  declarations: [CategoryChartComponent, TransactionTypeChartComponent],
   imports: [
     CommonModule,
     NgxChartsModule
   ],
-  exports: [CategoryChartComponent]
+  exports: [CategoryChartComponent, TransactionTypeChartComponent]
 })
 export class DataVisualizationModule {
 }
