8e3ea3071f130d3316504d38d74eb0c75adf9249
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-05 12:53:20 +0545

    Doc: Added utils doc

--- src/app/@core/utils/object.utils.ts
+++ src/app/@core/utils/object.utils.ts
@@ -1,18 +1,29 @@
 export class ObjectUtils {
+  /**
+   * @param input Any input.
+   * @returns Checks whether given input is `undefined` or `null`.
+   */
   static isEmpty(input: any) {
     return input === undefined || input === null;
   }
 
+  /**
+   * @param input Any input.
+   * @returns `undefined` if `input` is empty.
+   */
   public static setUndefinedIfNull(input: any): any {
     return this.isEmpty(input) ? undefined : input;
   }
 
+  /**
+   * @param input An object with property `_id`.
+   * @returns `undefined` if `input` is empty otherwise `_id` of `input`
+   */
   public static setUndefinedOrElseId(input: any): any {
     return this.isEmpty(input) ? undefined : input._id;
   }
 
   /**
-   *
    * @param obj A model object containing property and its value.
    * @returns A model object with non-empty property and its value.
    */
